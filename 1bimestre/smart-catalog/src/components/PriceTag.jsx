ecd34822299f0f69f12b28dafb80b0a002938e59
alexandrepetri <alexandrepetri@alunos.utfpr.edu.br>
2026-04-17 11:04:15 -0300

    👍

---
+++ 1bimestre/smart-catalog/src/components/PriceTag.jsx
@@ -0,0 +1,9 @@
+// src/components/PriceTag.jsx
+
+export const PriceTag = ({ value }) => {
+  return (
+    <span className="price-tag">
+      {value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+    </span>
+  );
+};
