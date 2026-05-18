6e4c84db77428d419f10d741b493947fd33823fd
alexandrepetri <alexandrepetri@alunos.utfpr.edu.br>
2026-05-18 15:49:14 -0300

    👍

---
+++ 2bimestre/smart-catalog-2/src/components/ProductGrid.jsx
@@ -0,0 +1,13 @@
+// src/components/ProductGrid.jsx
+
+import { ProductCard } from "./ProductCard";
+
+export const ProductGrid = ({ items }) => {
+  return (
+    <div className="product-grid">
+      {items.map((prod) => (
+        <ProductCard key={prod.id} info={prod} />
+      ))}
+    </div>
+  );
+};
